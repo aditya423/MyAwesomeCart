2605f847152d7d7d1d0d2985b2624a6491c88478
aditya423 <adityasghadge@gmail.com>
2021-10-10 23:50:49 +0530

    started

--- shop/views.py
+++ shop/views.py
@@ -3,27 +3,32 @@ from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
-def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides': nSlides, 'range': range(1,nSlides), 'product': products}
+def index(request): 
+    allProds = []
+    catProds = Product.objects.values('category','id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("Contact Us")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("Tracker Page")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("Search Page")
+    return render(request, 'shop/search.html')
 
 def productview(request):
-    return HttpResponse("Product View")
+    return render(request, 'shop/prodView.html')
 
 def checkout(request):
-    return HttpResponse("Checkout Page")
+    return render(request, 'shop/checkout.html')
